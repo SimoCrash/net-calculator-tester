e7de3c8d0821d6f0df025510d0d7851e7c664dd1
SimoCrash <simone.crescenzi@hotmail.com>
2023-04-03 18:03:05 +0200

    test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -4,16 +4,42 @@ namespace Tests
 {
     public class Tests
     {
-        Calculator calc;
+        Calculator calculator;
         [SetUp]
         public void Setup()
         {
+            calculator = new Calculator();
         }
 
         [Test]
-        public void Test1()
+        public void Test_Add()
         {
-            Assert.Pass();
+            var res = calculator.Add(2.5f, 7.5f);
+            //Assert.That(res, Is.EqualTo(10f));
+            Assert.AreEqual(10f, res, "Addizione errata");
         }
+
+        [Test]
+        public void Test_Subtract()
+        {
+            var res = calculator.Subtract(6.2f, 3.3f);
+            //Assert.That(res, Is.EqualTo(2.9f), "Sottrazione errata");
+            Assert.AreEqual(2.89999986f, res, "Addizione errata");
+        }
+
+        [Test]
+        public void Test_Divide()
+        {
+            var res = calculator.Divide(5.5f, 1.1f);
+            Assert.AreEqual(5f, res, "Divisione errata");
+        }
+
+        [Test]
+        public void Test_Multilpy()
+        {
+            var res = calculator.Multiply(1.7f, 4.2f);
+            Assert.AreEqual(7.14f, res, "Moltiplicazione errata");
+        }
+
     }
 }
